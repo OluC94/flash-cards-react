8f7a716c05a1ff02c70d322dda59812aa84248df
OluC94 <coker.olu94@gmail.com>
2024-06-27 11:21:49 +0100

    implements show/hide functionality

--- src/components/FlashCards.jsx
+++ src/components/FlashCards.jsx
@@ -6,12 +6,12 @@ export default function FlashCards() {
     const [cards, setCards] = useState(hiraganaData)
 
     const cardElements = hiraganaData.map((hiragana, index) => {
-        return <Card key={index} sound={hiragana.sound} character={hiragana.character}/>
+        return <Card key={index} sound={hiragana.sound} character={hiragana.character} showSound={hiragana.showSound}/>
     })
     return (
         <main >
             <h1>Hiragana Flashcards </h1>
-            <p className="flashcard-area">{cardElements}</p>
+            <div className="flashcard-area">{cardElements}</div>
         </main>
 
 )
